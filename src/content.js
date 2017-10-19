24568d0cf51d5779621f69c960a6326e5cc5a7c1
ForeverGlory <foreverglory@qq.com>
2017-10-20 01:16:31 +0800

    remove some log

--- src/content.js
+++ src/content.js
@@ -6,21 +6,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-console.log('Hello, Selection Popup WebExtensions.');
-
 import Vue from 'vue';
 import store from './js/store.js';
 import Popup from './Popup.vue';
 
-var images = {
-  loading: browser.extension.getURL("images/loading.gif")
-};
-
-document.onreadystatechange = function () {
-  if (document.readyState === "complete") {
-    console.log("complete");
-  }
-};
 browser.runtime.onMessage.addListener((message) => {
   return Promise.resolve({
     response: "Get it."
